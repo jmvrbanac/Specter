39ff950e9fe05502e4790a099d363f9392bbe7d4
John Vrbanac <john.vrbanac@linux.com>
2013-09-12 01:09:13 -0500

    Adding gettext call to strings

--- specter/expect.py
+++ specter/expect.py
@@ -1,5 +1,6 @@
 import inspect
 
+from specter import _
 from specter.spec import CaseWrapper
 
 
@@ -17,31 +18,31 @@ class ExpectAssert(object):
 
     @property
     def not_to(self):
-        self.actions.append('not')
+        self.actions.append(_('not'))
         self.used_negative = not self.used_negative
         return self.to
 
     @property
     def to(self):
-        self.actions.append('to')
+        self.actions.append(_('to'))
         return self
 
     def equal(self, expected):
-        self.actions.extend(['equal', str(expected)])
+        self.actions.extend([_('equal'), str(expected)])
         result = self.target == expected
         self.success = self._verify_condition(result)
 
         return self
 
     def be_greater_than(self, expected):
-        self.actions.extend(['be greater than', str(expected)])
+        self.actions.extend([_('be greater than'), str(expected)])
         result = self.target > expected
         self.success = self._verify_condition(result)
 
         return self
 
     def be_less_than(self, expected):
-        self.actions.extend(['be less than', str(expected)])
+        self.actions.extend([_('be less than'), str(expected)])
         result = self.target < expected
         self.success = self._verify_condition(result)
 
@@ -49,7 +50,7 @@ class ExpectAssert(object):
 
     def __str__(self):
         action_str = ' '.join(self.actions)
-        return 'expects {0}'.format(action_str)
+        return _('expects {0}').format(action_str)
 
 
 def expect(obj):
@@ -61,7 +62,7 @@ def expect(obj):
                 if type(wrapper) is CaseWrapper:
                     wrapper.expects.append(expect_obj)
     except Exception as error:
-        raise Exception('Error attempting to add expect to parent '
-                        'wrapper: {err}'.format(err=error))
+        raise Exception(_('Error attempting to add expect to parent '
+                        'wrapper: {err}').format(err=error))
 
     return expect_obj
