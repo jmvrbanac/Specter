f46ce284e715c2a9bcaefb76cc8c37be8b0eaee2
John Vrbanac <john.vrbanac@linux.com>
2013-09-15 16:18:17 -0500

    Attempting to fix python 3 gettext unicode arg issue

--- specter/__init__.py
+++ specter/__init__.py
@@ -1,5 +1,12 @@
 import gettext
-gettext.install('specter', unicode=1)
+import sys
+
+gettext_kwargs = {}
+if sys.version_info[0] < 3:
+    # Python 2 requires the unicode argument to be set
+    gettext_kwargs['unicode'] = 1
+
+gettext.install('specter', **gettext_kwargs)
 
 
 def _(msg):
