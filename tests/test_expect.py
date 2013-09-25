f01b88841c6126459fae87d8d2d44f7cbfb4bf46
John Vrbanac <john.vrbanac@linux.com>
2013-09-25 01:41:53 -0500

    Fixing expect tests

--- tests/test_expect.py
+++ tests/test_expect.py
@@ -5,57 +5,52 @@ from specter.expect import ExpectAssert, RequireAssert
 
 class TestExpectAssertion(TestCase):
 
+    def _create_assert(self, target):
+        return ExpectAssert(target)
+
     def test_creating_a_blank_reference(self):
         target = 'test'
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         self.assertEqual(expect.target, target)
 
     def test_expect_equal(self):
         target = 'test'
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         expect.to.equal(target)
         self.assertTrue(expect.success)
 
     def test_expect_not_to_equal(self):
         target = 'test'
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         expect.not_to.equal('nope')
         self.assertTrue(expect.success)
 
     def test_expect_greater_than(self):
         target = 100
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         expect.to.be_greater_than(99)
         self.assertTrue(expect.success)
 
     def test_expect_not_greater_than(self):
         target = 100
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         expect.not_to.be_greater_than(100)
         self.assertTrue(expect.success)
 
     def test_expect_less_than(self):
         target = 99
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         expect.to.be_less_than(100)
         self.assertTrue(expect.success)
 
     def test_expect_not_less_than(self):
         target = 99
-        expect = ExpectAssert(target)
+        expect = self._create_assert(target)
         expect.not_to.be_less_than(99)
         self.assertTrue(expect.success)
 
 
-class TestExpectAssertion(TestCase):
+class TestRequireAssertion(TestExpectAssertion):
 
-    def test_creating_a_blank_reference(self):
-        target = 'test'
-        expect = RequireAssert(target)
-        self.assertEqual(expect.target, target)
-
-    def test_expect_equal(self):
-        target = 'test'
-        expect = RequireAssert(target)
-        expect.to.equal(target)
-        self.assertTrue(expect.success)
+    def _create_assert(self, target):
+        return RequireAssert(target)
