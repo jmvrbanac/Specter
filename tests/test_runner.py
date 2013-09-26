082ab42d0751463da69b55c2424fa6f312b1fc93
John Vrbanac <john.vrbanac@linux.com>
2013-09-26 00:06:37 -0500

    Adding a couple quick and basic tests for the runner

---
+++ tests/test_runner.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from specter.runner import SpecterRunner
+
+
+class TestSpecterRunner(TestCase):
+
+    def setUp(self):
+        self.runner = SpecterRunner()
+
+    def test_ascii_art_generation(self):
+        """ We just want to know if it creates something"""
+        art = self.runner.generate_ascii_art()
+
+        self.assertGreater(len(art), 0)
+
+    def test_run(self):
+        self.runner.run(args=['--search', './tests/example_data', '--no-art'])
+        self.assertEqual(len(self.runner.suite_types), 1)
