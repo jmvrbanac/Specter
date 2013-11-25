eabc03529ca6142b95bdb07d9518961170f4e0c2
John Vrbanac <john.vrbanac@linux.com>
2013-11-24 23:06:57 -0600

    Bumping versions

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if os.path.exists('pypi_description.rst'):
 
 setup(
     name='Specter',
-    version='0.1.10',
+    version='0.1.11',
     packages=['specter'],
     url='https://github.com/jmvrbanac/Specter',
     license='MIT License',
