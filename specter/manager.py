a242146494c1a4c2f162219b033e4570fa3e2a9a
John Vrbanac <john.vrbanac@linux.com>
2013-12-23 20:30:59 -0600

    Finally have Multiprocess working... more to come

--- specter/manager.py
+++ specter/manager.py
@@ -1,56 +1,79 @@
 import multiprocessing as mp
 import threading
 import Queue
-import time
+from time import time, sleep
 from specter.spec import TestEvent
 
 
-def execute_test(work_queue, completed_queue, all_cases, all_parents):
-    while work_queue.qsize() > 0:
-        # Get item and get real function to execute
-        try:
-            case_wrapper = work_queue.get()
-            case_wrapper.case_func = all_cases[case_wrapper.case_func]
-            case_wrapper.parent = all_parents[case_wrapper.parent]
+class ExecuteTestProcess(mp.Process):
+    def __init__(self, work_queue, completed_queue, all_cases, all_parents):
+        super(ExecuteTestProcess, self).__init__()
+        self.work_queue = work_queue
+        self.completed_queue = completed_queue
+        self.all_cases = all_cases
+        self.all_parents = all_parents
 
-            #print pid, case_wrapper.case_func
-            case_wrapper.execute()
-            completed_queue.put_nowait(case_wrapper)
-        except:
-            break
-    return True
-
-
-def watch_completed(completed_queue):
-    while True:
-        while completed_queue.qsize() > 0:
-            wrapper = completed_queue.get_nowait()
-            if wrapper == 'KILL':
+    def run(self):
+        last_time = time()
+        completed = []
+        while True:
+            # Get item and get real function to execute
+            case_wrapper = self.work_queue.get()
+            if case_wrapper == 'STOP':
+                # Make sure buffer is cleared
+                if completed:
+                    self.completed_queue.put(completed)
+                    self.completed = []
                 return
 
-            all_cases = enumerate(wrapper.parent.cases)
-            for index, val in all_cases:
-                if wrapper.parent.cases[index].case_func == wrapper.case_func:
-                    wrapper.parent.cases[index] = val
+            case_wrapper.case_func = self.all_cases[case_wrapper.case_func]
+            case_wrapper.parent = self.all_parents[case_wrapper.parent]
+
+            case_wrapper.execute()
+            completed.append(case_wrapper)
+
+            # Flush completed buffer to queue
+            if completed and time() >= (last_time + 0.01):
+                self.completed_queue.put(completed)
+                completed = []
+                last_time = time()
+
+
+def watch_completed(completed_queue, all_cases, all_parents):
+    ticks = 0
+    while True:
+        wrapper_list = completed_queue.get()
+        ticks += 1
+        if wrapper_list == 'STOP':
+            return
+
+        for wrapper in wrapper_list:
+            parent_hash = wrapper.parent
+            func_hash = wrapper.case_func
+
+            wrapper.parent = all_parents[parent_hash]
+            wrapper.case_func = all_cases[func_hash]
+
+            # parent_cases = enumerate(wrapper.parent.cases)
+            # for index, val in parent_cases:
+            #     if val.case_func == wrapper.case_func:
+            #         wrapper.parent.cases[index] = wrapper
 
             wrapper.parent.top_parent.dispatch(TestEvent(wrapper))
-        time.sleep(0.5)
 
 
 class TestManager(object):
 
-    def __init__(self, num_processes=4):
+    def __init__(self, num_processes=6):
         self.processes = []
         self.num_processes = num_processes
-        self.manager = mp.Manager()
-        self.work_queue = self.manager.Queue()
-        self.completed = self.manager.Queue()
+        self.work_queue = mp.Queue()
+        self.completed = mp.JoinableQueue()
         self.case_functions = {}
         self.case_parents = {}
-        self.completed_thread_queue = Queue.Queue()
 
     def add_to_queue(self, case_wrapper):
-        self.work_queue.put_nowait(case_wrapper)
+        self.work_queue.put(case_wrapper)
 
         # Keep track of test functions
         key = hash(case_wrapper.case_func)
@@ -61,37 +84,40 @@ class TestManager(object):
         self.case_parents[key] = case_wrapper.parent
 
     def execute_all(self):
-        # Create monitor thread
-        self.monitor = threading.Thread(target=watch_completed,
-                                        args=(self.completed_thread_queue,))
-        self.monitor.start()
+        # Create monitor threads
+        self.monitors = []
+        for i in range(0, 6):
+            args = (self.completed,
+                    self.case_functions,
+                    self.case_parents)
+            monitor = threading.Thread(target=watch_completed, args=args)
+            self.monitors.append(monitor)
+            monitor.start()
 
-        args = (self.work_queue, self.completed, self.case_functions,
-                self.case_parents)
         for i in range(0, self.num_processes):
-            test_process = mp.Process(target=execute_test, args=args)
-            test_process.daemon = True
+            test_process = ExecuteTestProcess(
+                self.work_queue, self.completed, self.case_functions,
+                self.case_parents)
             self.processes.append(test_process)
+            self.work_queue.put('STOP')
             test_process.start()
-            test_process.run()
-
-        # Manually block and resolve completed tests
-        while True in [proc.is_alive() for proc in self.processes]:
-            while self.completed.qsize() > 0:
-                wrapper = self.completed.get()
-                parent_hash = wrapper.parent
-                func_hash = wrapper.case_func
-
-                wrapper.parent = self.case_parents[parent_hash]
-                wrapper.case_func = self.case_functions[func_hash]
-                self.completed_thread_queue.put(wrapper)
-            time.sleep(0.5)
 
         # Join already completed processes for good measure
         for test_process in list(self.processes):
             test_process.join()
             self.processes.remove(test_process)
 
+        # Adding stop in separate for to avoid a queue race condition
+        for monitor in self.monitors:
+            self.completed.put('STOP')
+
+        # Make sure we wait until the queue has been emptied
+        while self.completed.qsize() > 0:
+            sleep(0.1)
+
+        self.completed.close()
+        self.completed.join_thread()
+
         # Join monitor thread
-        self.completed_thread_queue.put('KILL')
-        self.monitor.join()
+        for monitor in self.monitors:
+            monitor.join()
--- specter/reporting/dots.py
+++ specter/reporting/dots.py
@@ -7,6 +7,10 @@ from specter.reporting import AbstractConsoleReporterPlugin
 
 class DotsReporter(AbstractConsoleReporterPlugin):
 
+    def __init__(self):
+        super(DotsReporter, self).__init__()
+        self.total = 0
+
     def get_name(self):
         return _('Dots Reporter')
 
@@ -14,9 +18,10 @@ class DotsReporter(AbstractConsoleReporterPlugin):
         describe.add_listener(TestEvent.COMPLETE, self.test_event)
 
     def test_event(self, evt):
+        self.total += 1
         char = '.' if evt.payload.success else 'x'
         stdout.write(char)
         stdout.flush()
 
     def print_summary(self):
-        print(_('\nTest Run Complete!'))
+        print(_('\n{0} Test(s) completed!'.format(self.total)))
