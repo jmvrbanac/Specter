11ad844940567a074974ae13aba14b106c7fb853
John Vrbanac <john.vrbanac@linux.com>
2013-12-29 00:50:03 -0600

    Printing out error information for the dots reporter

--- specter/reporting/dots.py
+++ specter/reporting/dots.py
@@ -4,6 +4,8 @@ from specter import _
 from specter.spec import TestEvent
 from specter.reporting import AbstractConsoleReporter
 from specter.reporting import AbstractParallelReporter
+from specter.reporting.console import print_test_msg, print_test_args
+from specter.reporting.console import print_expects, TestStatus
 
 
 class DotsReporter(AbstractConsoleReporter, AbstractParallelReporter):
@@ -11,6 +13,7 @@ class DotsReporter(AbstractConsoleReporter, AbstractParallelReporter):
     def __init__(self):
         super(DotsReporter, self).__init__()
         self.total = 0
+        self.failed_tests = []
 
     def get_name(self):
         return _('Dots Reporter')
@@ -18,11 +21,40 @@ class DotsReporter(AbstractConsoleReporter, AbstractParallelReporter):
     def subscribe_to_describe(self, describe):
         describe.add_listener(TestEvent.COMPLETE, self.test_event)
 
+    def print_error(self, wrapper):
+        """ A crude way of output the errors for now. This needs to be
+        cleaned up into something better.
+        """
+        level = 0
+        parent = wrapper.parent
+        while parent:
+            print_test_msg(parent.name, level, TestStatus.FAIL)
+            level += 1
+            parent = parent.parent
+
+        print_test_msg(wrapper.name, level, TestStatus.FAIL)
+        print_test_args(wrapper.execute_kwargs, level, TestStatus.FAIL)
+
+        if wrapper.error:
+            for line in wrapper.error:
+                print_test_msg(line, level+2, TestStatus.FAIL)
+
+        print_expects(wrapper, level)
+
     def test_event(self, evt):
         self.total += 1
-        char = '.' if evt.payload.success else 'x'
+        if evt.payload.success:
+            char = '.'
+        else:
+            char = 'x'
+            self.failed_tests.append(evt.payload)
         stdout.write(char)
         stdout.flush()
 
     def print_summary(self):
-        print(_('\n{0} Test(s) completed!'.format(self.total)))
+        print(_('\n{0} Test(s) Executed!'.format(self.total)))
+
+        if len(self.failed_tests) > 0:
+            print(_('\nFailed test information:'))
+            for wrapper in self.failed_tests:
+                self.print_error(wrapper)
