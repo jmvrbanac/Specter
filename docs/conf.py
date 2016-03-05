cce8fdd6aa48c2c4ac5de3b2ad740c1197de3023
John Vrbanac <john.vrbanac@linux.com>
2016-03-05 15:06:56 -0600

    Bumping version to 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Specter'
-copyright = u'2013, John Vrbanac'
+copyright = u'2013-2016, John Vrbanac'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
