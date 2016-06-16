3c2f33e51904d0d03c270b1000bcec9dd1f382a8
Mark Church <mark.church@emc.com>
2016-06-16 11:30:18 -0500

    Fix flake8 error

--- tests/test_reporter.py
+++ tests/test_reporter.py
@@ -10,6 +10,7 @@ from specter.spec import DescribeEvent, Describe
 import os
 import xml.etree.ElementTree as ET
 
+
 @pytest.fixture()
 def pass_capfd(request, capfd):
     """Provide capfd object to unittest.TestCase instances"""
