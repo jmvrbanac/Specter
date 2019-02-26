5baddca7475d73b13a6e2520c5263a5556364a09
John Vrbanac <john.vrbanac@linux.com>
2019-02-26 00:19:06 -0600

    Defaulting log level to info

--- specter/logger.py
+++ specter/logger.py
@@ -3,7 +3,7 @@ import logging
 
 def setup(cfg=None):
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         # format='%(name)s - %(message)s'
         format='%(message)s'
     )
