5e90a2f2a15278aeebf7b60d7cc617fbfa930906
Russ MacShane <rmacshane@liquidweb.com>
2020-01-09 15:36:30 -0600

    MAD-1513: Added more attributes, some are placeholders for now

--- specter/reporting/xunit.py
+++ specter/reporting/xunit.py
@@ -45,8 +45,37 @@ class XUnitTestSuite(object):
     def __init__(self, suite):
         self.suite = suite
 
+    @property
+    def name(self):
+        return self.suite.name
+
+    @property
+    def time(self):
+        return "N/A"
+
+    @property
+    def errors(self):
+        return str(len([case for case in self.suite.cases if case.errors]))
+
+    @property
+    def failures(self):
+        return str(len([case for case in self.suite.cases if not case.successful]))
+
+    @property
+    def skipped(self):
+        return str(len([case for case in self.suite.cases if case.skipped or case.incomplete]))
+
+    @property
+    def tests(self):
+        return str(len(self.suite.cases))
+
     def convert_to_xml(self):
-        element = Element('testsuite', {'name': self.suite.name})
+        element = Element('testsuite', {'name': self.name,
+                                        'time': self.time,
+                                        'errors': self.errors,
+                                        'failures': self.failures,
+                                        'skipped': self.skipped,
+                                        'tests': self.tests})
 
         for case in self.suite.cases:
             test_case = XUnitTestCase(case)
@@ -59,8 +88,22 @@ class XUnitTestCase(object):
     def __init__(self, case):
         self.case = case
 
+    @property
+    def name(self):
+        return self.case.raw_name
+
+    @property
+    def classname(self):
+        return "N/A"
+
+    @property
+    def time(self):
+        return "N/A"
+
     def convert_to_xml(self):
-        element = Element('testcase', {'name': self.case.raw_name})
+        element = Element('testcase', {'name': self.name,
+                                       'classname': self.classname,
+                                       'time': self.time})
         return element
 
 
